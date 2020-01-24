2689bf5886920bc5ce3b8541b2852c38cde4e92e
Zver <tsydenov.zanabazar@gmail.com>
2020-01-24 19:49:16 +0700

    Add Vuex for storing state

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -5,23 +5,25 @@
             {{ message.text }}
         </v-card-text>
         <v-card-actions>
-            <v-btn value="Edit" @click="edit" small flat round>Edit</v-btn>
-            <v-btn value="Delete" @click="del" small flat round>Delete</v-btn>
+            <v-btn value="Edit" @click="edit" small text rounded>Edit</v-btn>
+            <v-btn value="Delete" @click="del" small text rounded>Delete</v-btn>
         </v-card-actions>
     </v-card>
 </template>
 
 <script>
+    import { mapActions } from'vuex'
     import { mdiDelete } from '@mdi/js'
 
     export default {
-        props: ['message', 'editMessage', 'deleteMessage', 'messages'],
+        props: ['message', 'editMessage'],
         methods: {
+            ...mapActions(['removeMessageAction']),
             edit() {
                 this.editMessage(this.message)
             },
             del() {
-                this.deleteMessage(this.message)
+                this.removeMessageAction(this.message)
             }
         }
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
+import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
 import { mdiExitRun } from '@mdi/js'
@@ -15,5 +17,6 @@ Vue.use(Vuetify, { iconfont: 'mdiSvg' })
 new Vue({
     el: '#app',
     vuetify: new Vuetify({}),
+    store,
     render: a => a(App)
 })
